475758dd9518dc716f0bb9abd0d6babef2076f06
skylor727 <skylor1997@hotmail.com>
2022-02-08 16:31:21 -0700

    add functionality for feeding finches

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
 from dataclasses import fields
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
+from .forms import FeedingForm
 
 # Create your views here.
 from django.http import HttpResponse
@@ -18,7 +19,8 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
-    return render(request, 'finches/detail.html', { 'finch': finch })
+    feeding_form = FeedingForm()
+    return render(request, 'finches/detail.html', { 'finch': finch, 'feeding_form': feeding_form })
 
 class FinchCreate(CreateView):
     model = Finch
@@ -31,3 +33,13 @@ class FinchUpdate(UpdateView):
 class FinchDelete(DeleteView):
     model = Finch
     success_url = '/finches/'
+
+def add_feeding(request, finch_id):
+    form = FeedingForm(request.POST)
+
+    if form.is_valid():
+        #dont save form until the finch id is assigned
+        new_feeding = form.save(commit=False)
+        new_feeding.finch_id = finch_id
+        new_feeding.save()
+    return redirect('detail', finch_id=finch_id)
